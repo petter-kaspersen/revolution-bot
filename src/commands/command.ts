4a54bf5c4796152b2e2270a7718944fc51b891f3
Petter Kaspersen <petter@leetify.com>
2024-08-18 16:56:25 +0200

    Give treat to other ppl

--- src/commands/command.ts
+++ src/commands/command.ts
@@ -24,10 +24,14 @@ export default class Command {
   }
 
   async init() {
+    const commandWithoutArgs = this.name.replace(/<[^>]+>/g, "").trim();
+
     this.bot.on("messageCreate", async (message: Message) => {
       if (
         message.author.bot ||
-        !message.content.toLowerCase().startsWith(this.prefix + this.name)
+        !message.content
+          .toLowerCase()
+          .startsWith(this.prefix + commandWithoutArgs)
       )
         return;
 
@@ -39,19 +43,21 @@ export default class Command {
         return;
       }
 
-      // Create user if it does not exist
-      await prisma.user.upsert({
-        where: {
-          discordId: message.author.id,
-        },
-        create: {
-          discordId: message.author.id,
-          username: message.member?.displayName || message.author.username,
-        },
-        update: {
-          username: message.member?.displayName || message.author.username,
-        },
-      });
+      for (const user of [message.author, ...message.mentions.users.values()]) {
+        // Create user if it does not exist
+        await prisma.user.upsert({
+          where: {
+            discordId: user.id,
+          },
+          create: {
+            discordId: user.id,
+            username: user.displayName || user.username,
+          },
+          update: {
+            username: user.displayName || user.username,
+          },
+        });
+      }
 
       Logger.Info(
         `Command ${this.prefix}${this.name} executed by ${message.author.username}`
--- src/commands/treat.ts
+++ src/commands/treat.ts
@@ -2,46 +2,97 @@ import { Client, Message } from "discord.js";
 import Command from "./command";
 import prisma from "../db";
 
+const isLessThan60Seconds = (lastTreated: Date) => {
+  return lastTreated.getTime() > Date.now() - 60000;
+};
+
 export class CommandTreat extends Command {
   constructor(bot: Client) {
-    super(bot, "treat", "Give a treat to the revolting peasants");
+    super(bot, "treat <?person>", "Give a treat to the revolting peasants");
   }
 
   async action(message: Message) {
-    const user = await prisma.user.findUniqueOrThrow({
+    let user = await prisma.user.findUniqueOrThrow({
       where: {
         discordId: message.author.id,
       },
     });
 
-    const lastTreated = user.lastTreated;
+    const hasMentionedAnotherUser = message.mentions.users.size > 0;
+
+    if (hasMentionedAnotherUser) {
+      const mentionedUser = message.mentions.users.first();
+
+      if (mentionedUser?.id === message.author.id) {
+        return message.reply("You can't treat yourself! 🍬");
+      }
+    }
+
+    const lastTreated = hasMentionedAnotherUser
+      ? user.lastGivenTreat
+      : user.lastTreated;
 
     // Check if last treated is less than 60 seconds ago
-    if (lastTreated && lastTreated.getTime() > Date.now() - 60000) {
-      message.reply(
-        `You can only treat the bot once every 60 seconds. You can try again in ${Math.ceil(
-          (lastTreated.getTime() + 60000 - Date.now()) / 1000
-        )} seconds.`
-      );
+    if (lastTreated && isLessThan60Seconds(lastTreated)) {
+      const replyMessage = hasMentionedAnotherUser
+        ? `You can only treat someone else once every 60 seconds. You can try again in ${Math.ceil(
+            (lastTreated.getTime() + 60000 - Date.now()) / 1000
+          )} seconds.`
+        : `You can only treat the bot once every 60 seconds. You can try again in ${Math.ceil(
+            (lastTreated.getTime() + 60000 - Date.now()) / 1000
+          )} seconds.`;
+
+      message.reply(replyMessage);
       return;
     }
 
-    const newUser = await prisma.user.update({
-      where: {
-        discordId: message.author.id,
-      },
-      data: {
-        treats: {
-          increment: 1,
+    let newUser;
+
+    if (hasMentionedAnotherUser) {
+      newUser = await prisma.user.update({
+        where: {
+          discordId: user.discordId,
         },
-        lastTreated: new Date(),
-        revolutionStreak: 0,
-        givenTreats: true,
-      },
-    });
+        data: {
+          treats: {
+            increment: 1,
+          },
+          givenTreats: true,
+        },
+      });
+
+      await prisma.user.update({
+        where: {
+          discordId: message.author.id,
+        },
+        data: {
+          lastGivenTreat: new Date(),
+        },
+      });
+    } else {
+      newUser = await prisma.user.update({
+        where: {
+          discordId: user.discordId,
+        },
+        data: {
+          treats: {
+            increment: 1,
+          },
+          lastTreated: new Date(),
+          revolutionStreak: 0,
+          givenTreats: true,
+        },
+      });
+    }
 
     message.reply(
-      `Nice job, ${user?.username}, you gave a treat to the all-seeing revolution overlord! You're now at ${newUser.treats} treats.  🍬`
+      `Nice job, ${user?.username}, you gave a treat to ${
+        hasMentionedAnotherUser
+          ? `<@${message.mentions.users.first()?.id}>`
+          : "the all-seeing revolution overlord"
+      }! ${hasMentionedAnotherUser ? "They are" : "You are"} now at ${
+        newUser.treats
+      } treats.  🍬`
     );
   }
 }
